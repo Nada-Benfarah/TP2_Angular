f6daf17cbff1bd5f084a575e625108421355a1b3
unknown <nadabenfarah3@gmail.com>
2024-05-05 13:31:16 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
-
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CarteVisiteComponent
+    CarteVisiteComponent,
+    DataBindingComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  nom="nada";
+  surname="benfarah"
+
+
+updateName(newName:string) {
+console.log(newName)
+this.surname = newName.toString();
+console.log(this.nom);
+}
+getSuName() {
+return this.surname;
+}
+getName() {
+return this.nom;
+}
+
+
+  message: string = 'Bonjour, monde!';
+
+   color="blue";
+
+  changerMessage(): void {
+    this.message = 'Helloo ,benfarah nada!';
+    }
+
+
+
 
 }
